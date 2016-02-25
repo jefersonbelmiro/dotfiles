83426d8b7ed73372ec7a8cd28e4b5b6bdef3b877
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2016-02-24 21:41:20 -0300

    add install config

---
+++ init.vim
@@ -0,0 +1,18 @@
+call plug#begin('~/.config/nvim/plugged')
+
+" Make sure you use single quotes
+
+" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+" Plug 'junegunn/vim-easy-align'
+
+" Group dependencies, vim-snippets depends on ultisnips
+Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+
+" On-demand loading
+Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+
+" Plugin outside ~/.vim/plugged with post-update hook
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+
+" Add plugins to &runtimepath
+call plug#end()
---
+++ install.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+INSTALL_GIT=true
+INSTALL_TREE=true
+INSTALL_EXUBERANT_CTAGS=true
+INSTALL_AG=true
+INSTALL_NVIM=true
+
+CONFIGURE_NVIM=true
+CONFIGURE_GIT=true
+
+path_nvim="$HOME/.config/nvim"
+
+command_exists () {
+    type "$1" > /dev/null ;
+}
+
+# git 
+if command_exists git; then
+    echo "git already installed"
+elif $INSTALL_GIT; then
+
+    echo "install git"
+
+    sudo apt-get install git
+    git config --global user.name 'Jeferson Belmiro'
+    git config --global user.email 'jeferson.belmiro@gmail.com'
+else
+    echo "skiping install git"
+fi
+
+if command_exists git && $CONFIGURE_GIT ; then
+
+    # git auth cache
+    git config --global credential.helper "cache --timeout=10800"
+
+    # git diff com vim
+    git config --global pager.diff false
+    git config --global difftool.prompt false
+    git config --global diff.external git_diff_wrapper
+    sudo ln -s ~/dotfiles/bin/git_diff_wrapper /usr/local/bin/
+
+fi
+
+# tree
+if command_exists tree; then
+    echo "tree already installed"
+elif $INSTALL_TREE; then 
+    sudo apt-get install tree
+else
+    echo "skiping install tree"
+fi
+
+# exuberant-ctags
+if command_exists exuberant-ctags; then
+    echo "exuberant-ctags already installed"
+elif $INSTALL_EXUBERANT_CTAGS; then
+    echo "install exuberant-ctags"
+    sudo apt-get install exuberant-ctags
+else 
+    echo "skiping install exuberant-ctags"
+fi
+
+# ag - the silver search
+#
+# plugin vim
+# - https://github.com/rking/ag.vim
+# programa
+# - https://github.com/ggreer/the_silver_searcher
+#
+if command_exists ag; then
+    echo "ag already installed"
+elif $INSTALL_AG; then
+    echo "install ag"
+    sudo apt-get install python-software-properties
+    sudo apt-add-repository ppa:mizuno-as/silversearcher-ag
+    sudo apt-get update
+    sudo apt-get install silversearcher-ag
+else 
+    echo "skiping install ag"
+fi
+
+# neovim
+# https://github.com/neovim/neovim/wiki/Installing-Neovim
+if command_exists nvim; then
+    echo "nvim already installed"
+elif $INSTALL_NVIM; then
+    echo "install nvim"
+
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install neovim
+    sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+    # For Python 2 plugins,
+    sudo pip2 install neovim
+    # For Python 3 plugins
+    sudo pip3 install neovim
+else
+    echo "skiping install nvim"
+fi
+
+if command_exists nvim && $CONFIGURE_NVIM; then
+    echo "setting nvim"
+
+    if [ -d "$path_nvim" ]; then
+        echo "neovim directory exists: $path_nvim, remove first"
+        exit 1;
+    fi
+
+    mkdir ~/.config/nvim
+    cp init.vim ~/.config/nvim/init.vim
+
+    # vim-plug for neovim
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    nvim +PlugInstall
+fi 
+
+echo "installation complete."
