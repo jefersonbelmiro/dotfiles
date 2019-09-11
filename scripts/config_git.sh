c2b9df7d1c5a58fc77a452f2845f15234f3ca0be
Jeferson Belmiro <jeferson.belmiro@dbserver.com.br>
2019-09-11 15:13:04 -0300

    fix

--- scripts/config_git.sh
+++ scripts/config_git.sh
@@ -9,4 +9,5 @@ git config --global merge.tool "vimdiff3"
 git config --global mergetool.prompt false
 git config --global core.pager 'less -r'
 git config --global alias.lg "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.taglast "! git tag | sort -r --version-sort | head -n1"
 
