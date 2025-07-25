4e94cfbaa9ed50a15efa03bc5c59f2a02bc9cd17
Jeferson Belmiro <jeferson.belmiro@cwi.com.br>
2025-07-25 10:41:32 -0300

    feat: change lualine style

--- vim/lua/plugins/lualine.lua
+++ vim/lua/plugins/lualine.lua
@@ -1,129 +1,62 @@
+-- Example configuration
+local function get_mode_abbreviation()
+  -- Map of modes to their respective shorthand indicators
+  local mode_map = {
+    n = "N", -- Normal mode
+    i = "I", -- Insert mode
+    v = "V", -- Visual mode
+    [""] = "V", -- Visual block mode
+    V = "V", -- Visual line mode
+    c = "C", -- Command-line mode
+    no = "N", -- NInsert mode
+    s = "S", -- Select mode
+    S = "S", -- Select line mode
+    ic = "I", -- Insert mode (completion)
+    R = "R", -- Replace mode
+    Rv = "R", -- Virtual Replace mode
+    cv = "C", -- Command-line mode
+    ce = "C", -- Ex mode
+    r = "R", -- Prompt mode
+    rm = "M", -- More mode
+    ["r?"] = "?", -- Confirm mode
+    ["!"] = "!", -- Shell mode
+    t = "T", -- Terminal mode
+  }
+  -- Return the mode shorthand or [UNKNOWN] if no match
+  return mode_map[vim.fn.mode()] or "[UNKNOWN]"
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        -- set an empty statusline till lualine loads
-        vim.o.statusline = " "
-      else
-        -- hide the statusline on the starter page
-        vim.o.laststatus = 0
-      end
-    end,
-    opts = function()
-      -- PERF: we don't need this lualine require madness 🤷
-      local lualine_require = require("lualine_require")
-      lualine_require.require = require
-
-      local icons = LazyVim.config.icons
-
-      vim.o.laststatus = vim.g.lualine_laststatus
-
-      local opts = {
-        options = {
-          theme = "auto",
-          globalstatus = vim.o.laststatus == 3,
-          disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-
-          lualine_c = {
-            LazyVim.lualine.root_dir(),
-            {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { LazyVim.lualine.pretty_path() },
-          },
-          lualine_x = {
-            Snacks.profiler.status(),
-          -- stylua: ignore
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = {
           {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
-            },
-          },
-          lualine_y = {
-            { "progress", separator = " ", padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
+            "mode",
+            fmt = get_mode_abbreviation,
+            separator = { left = "" },
+            right_padding = 2,
+            --color = { bg = "None" },
           },
         },
-        extensions = { "neo-tree", "lazy", "fzf" },
-      }
-
-      -- do not add trouble symbols if aerial is enabled
-      -- And allow it to be overriden for some buffer types (see autocmds)
-      if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-        local trouble = require("trouble")
-        local symbols = trouble.statusline({
-          mode = "symbols",
-          groups = {},
-          title = false,
-          filter = { range = true },
-          format = "{kind_icon}{symbol.name:Normal}",
-          hl_group = "lualine_c_normal",
-        })
-        table.insert(opts.sections.lualine_c, {
-          symbols and symbols.get,
-          cond = function()
-            return vim.b.trouble_lualine ~= false and symbols.has()
-          end,
-        })
-      end
-
-      return opts
-    end,
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          -- {
+          --   function()
+          --     return " " .. os.date("%R")
+          --   end,
+          --   separator = { right = "" },
+          -- },
+        },
+      },
+    },
   },
 }
